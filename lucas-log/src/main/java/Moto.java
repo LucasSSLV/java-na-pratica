898209eeb7774ac3f000b5f8c3b1adb9165eafe3
Lucas santos da silva <lukinhas.kell@hotmail.com>
2024-09-10 20:32:53 -0300

    add classe Moto que extende Veiculo

---
+++ lucas-log/src/main/java/Moto.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Moto extends Veiculo {
+    public Moto(String placa, int anoFabricacao, String combiustivel, List<String> abastecimentos) {
+        super(placa, anoFabricacao, combiustivel, abastecimentos);
+    }
+}
