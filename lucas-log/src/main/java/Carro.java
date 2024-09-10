4ebf23b07b0f7d25ddbd0ffe27263f06705b818e
Lucas santos da silva <lukinhas.kell@hotmail.com>
2024-09-10 20:27:50 -0300

    add classe abstrata Veiculo e classe Carro

--- lucas-log/src/main/java/Carro.java
+++ lucas-log/src/main/java/Carro.java
@@ -1,2 +1,8 @@
-public class Carro {
+import java.util.List;
+
+public class Carro extends Veiculo {
+
+    public Carro(String placa, int anoFabricacao, String combiustivel, List<String> abastecimento) {
+        super(placa, anoFabricacao, combiustivel, abastecimento);
+    }
 }
--- lucas-log/src/main/java/Veiculo.java
+++ lucas-log/src/main/java/Veiculo.java
@@ -4,6 +4,44 @@ public abstract class Veiculo {
     String placa;
     int anoFabricacao;
     String combiustivel;
-    List<String> abastecimento;
+    List<String> abastecimentos;
 
+    public Veiculo(String placa, int anoFabricacao, String combiustivel, List<String> abastecimentos) {
+        this.placa = placa;
+        this.anoFabricacao = anoFabricacao;
+        this.combiustivel = combiustivel;
+        this.abastecimentos = abastecimentos;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public String getCombiustivel() {
+        return combiustivel;
+    }
+
+    public void setCombiustivel(String combiustivel) {
+        this.combiustivel = combiustivel;
+    }
+
+    public List<String> getAbastecimentos() {
+        return abastecimentos;
+    }
+
+    public void setAbastecimentos(List<String> abastecimento) {
+        this.abastecimentos = abastecimento;
+    }
 }
